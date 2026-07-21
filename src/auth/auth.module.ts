fcc90526742df6b97f38f5c50d21edceefa6c953
Susmita Dhital <susmitadhital7@gmail.com>
2026-07-22 08:07:49 +1000

    wip auth modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PasswordService } from './password.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [PasswordService],
+  controllers: [AuthController],
+  providers: [PasswordService, AuthService],
   exports: [PasswordService],
 })
 export class AuthModule {}
